9b9862c472bd8007ac83d9bea42a8fa0120d1186
sharoncy <s.c.yates@medisin.uio.no>
2023-05-09 12:55:10 +0200

    Restructure and cleanup code, OOP

---
+++ PyNutil/main.py
@@ -0,0 +1,14 @@
+
+
+
+
+class PyNutil:
+    def __init__(self, segmentation_folder) -> None:
+        self.segmentation_folder = segmentation_folder
+        self.json_file = json_file
+        self.colour = colour
+        self.output_path = output_path
+
+    def build_quantifier(self):
+        #do all the expensive computations
+
