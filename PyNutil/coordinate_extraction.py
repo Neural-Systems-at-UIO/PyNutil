eedb4afaa05be80d80ae2ca73667830dea2e5cf8
sharoncy <s.c.yates@medisin.uio.no>
2023-05-11 11:21:22 +0200

    Create PixelCountPerRegion function

--- PyNutil/coordinate_extraction.py
+++ PyNutil/coordinate_extraction.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import pandas as pd
 from DeepSlice.coord_post_processing.spacing_and_indexing import number_sections
 import json
 from read_and_write import loadVisuAlignJson
@@ -269,4 +270,41 @@ def labelPoints(points, label_volume, scale_factor=1):
     #get the label value for each point
     labels = label_volume[x,y,z]
     return labels
+
+# related to object_counting
+# consider separating out writing to CSV in future
+def PixelCountPerRegion(labelsDict, label_colours, output_csv): 
+    """Function for counting no. of pixels per region and writing to CSV based on 
+    a dictionary with the region as the key and the points as the value, """
+    counted_labels, label_counts = np.unique(labelsDict, return_counts=True)
+    # which regions have pixels, and how many pixels are there per region
+    counts_per_label = list(zip(counted_labels,label_counts))
+    # create a list of unique regions and pixel counts per region
+
+    df_counts_per_label = pd.DataFrame(counts_per_label, columns=["allenID","pixel count"])
+    # create a pandas df with regions and pixel counts
+
+    df_label_colours =pd.read_csv(label_colours, sep=",")
+    # find colours corresponding to each region ID and add to the pandas dataframe
+
+    #look up name, r, g, b in df_allen_colours in df_counts_per_label based on "allenID"
+    new_rows = []
+    for index, row in df_counts_per_label.iterrows():
+        mask = df_label_colours["allenID"] == row["allenID"] 
+        current_region_row = df_label_colours[mask]
+        current_region_name = current_region_row["name"].values
+        current_region_red = current_region_row["r"].values
+        current_region_green = current_region_row["g"].values
+        current_region_blue = current_region_row["b"].values
+
+        row["name"]  = current_region_name[0]
+        row["r"] = current_region_red[0]
+        row["g"] = current_region_green[0]
+        row["b"] = current_region_blue[0]
+        
+        new_rows.append(row)
+
+    df_counts_per_label_name = pd.DataFrame(new_rows)
+    
+    df_counts_per_label_name.to_csv(output_csv, sep=";", na_rep='', index= False)
 
