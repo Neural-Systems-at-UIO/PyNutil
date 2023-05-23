922d1c0882eaf8f096b8dfbaa994a1f66e33dda4
Harry Carey <harry.carey95@gmail.com>
2023-05-23 17:46:21 +0200

    reformatted with black and added metadata loader

--- PyNutil/dream_workflow.py
+++ PyNutil/dream_workflow.py
@@ -1,30 +1,27 @@
 import PyNutil
 
-#define parameters
-#specify loacation of segmentation folder
+# define parameters
+# specify loacation of segmentation folder
 segmentation_folder = r"blabla/blabla"
-#specify location of json file
+# specify location of json file
 json_file = r"blabla/blabla.json"
-#specify colour to quantify
+# specify colour to quantify
 colour = [255, 255, 255]
-#specify output location
+# specify output location
 output_path = r"blabla/blabla/output"
 
-quantifier = PyNutil(segmentation_folder,
-                                    json_file,
-                                    colour,
-                                    output_path)
+quantifier = PyNutil(segmentation_folder, json_file, colour, output_path)
 
 quantifier.build_quantifier()
 
-#define your mask as either a png, or a qcalign damage map
-#this mask will be applied to all 
+# define your mask as either a png, or a qcalign damage map
+# this mask will be applied to all
 quantifier.load_mask(mask_path=r"blablabla/")
 
-#load a custom region file
+# load a custom region file
 quantifier.load_custom_regions(custom_region_json=r"blablabla/")
-#run coordinate extraction
-#ideally extract coordinates per section and whole brain
+# run coordinate extraction
+# ideally extract coordinates per section and whole brain
 points = quantifier.get_coordinates()
 
 quantifier.save_coordinates()
--- PyNutil/main.py
+++ PyNutil/main.py
@@ -1,14 +1,27 @@
-
-
+from .metadata import metadata_loader
+from read_and_write import readAtlasVolume
 
 
 class PyNutil:
-    def __init__(self, segmentation_folder) -> None:
+    def __init__(
+        self,
+        segmentation_folder,
+        json_file,
+        colour,
+        atlas,
+    ) -> None:
+        self.config, self.metadata_path = metadata_loader.load_config()
+        if atlas not in self.config["annotation_volumes"]:
+            raise ValueError(
+                f"Atlas {atlas} not found in config file\n valid atlases are {self.config['annotation_volumes'].keys()}"
+            )
         self.segmentation_folder = segmentation_folder
         self.json_file = json_file
         self.colour = colour
-        self.output_path = output_path
+        self.atlas = atlas
+        # load the metadata json as well as the path to stored data files
 
     def build_quantifier(self):
-        #do all the expensive computations
-
+        # do all the expensive computations
+        atlas_path = self.config["annotation_volumes"][self.atlas]["volume"]
+        self.atlas_volume = readAtlasVolume(self.atlas)
