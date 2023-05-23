d6b1387ee1f4c2caa5af7620b7eefa3a058576e2
sharoncy <s.c.yates@medisin.uio.no>
2023-05-23 19:37:00 +0200

    Testing

--- PyNutil/counting_and_load.py
+++ PyNutil/counting_and_load.py
@@ -23,14 +23,14 @@ def labelPoints(points, label_volume, scale_factor=1):
 
 # related to counting_and_load
 def PixelCountPerRegion(labelsDict, label_colours): 
-    """Function for counting no. of pixels per region and writing to CSV based on 
+    """Function for counting no. of pixels per region per brain and writing to CSV based on 
     a dictionary with the region as the key and the points as the value, """
     counted_labels, label_counts = np.unique(labelsDict, return_counts=True)
     # which regions have pixels, and how many pixels are there per region
     counts_per_label = list(zip(counted_labels,label_counts))
     # create a list of unique regions and pixel counts per region
 
-    df_counts_per_label = pd.DataFrame(counts_per_label, columns=["idx","pixel_count"])
+    df_counts_per_label = pd.DataFrame(counts_per_label, columns=["idx","object_area"])
     # create a pandas df with regions and pixel counts
 
     df_label_colours =pd.read_csv(label_colours, sep=",")
@@ -57,36 +57,6 @@ def PixelCountPerRegion(labelsDict, label_colours):
     return df_counts_per_label_name
 
 
-"""read flat file and write into an np array"""
-def flat_to_array(flatfile):
-    with open(flatfile,"rb") as f:
-        #i dont know what b is, w and h are the width and height that we get from the 
-        #flat file header
-        b,w,h=struct.unpack(">BII",f.read(9))
-        #data is a one dimensional list of values
-        #it has the shape width times height
-        data =struct.unpack(">"+("xBH"[b]*(w*h)),f.read(b*w*h))
-
-    #convert flat file data into an array, previously data was a tuple
-    imagedata = np.array(data)
-
-    #create an empty image array in the right shape, write imagedata into image_array
-    image = np.zeros((h,w))
-    for x in range(w):
-        for y in range(h):
-            image[y,x] = imagedata[x+y*w]
-
-    image_arr = np.array(image)
-    return image_arr
 
 
-# import flat files, count pixels per label, np.unique... etc. nitrc.org/plugins/mwiki/index.php?title=visualign:Deformation
 
-"""
-   base=slice["filename"][:-4]
-   
-   import struct
-   with open(base+".flat","rb") as f:
-       b,w,h=struct.unpack(">BII",f.read(9))
-       data=struct.unpack(">"+("xBH"[b]*(w*h)),f.read(b*w*h))
-"""
