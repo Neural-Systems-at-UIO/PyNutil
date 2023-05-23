938b280edb32784ebbe2d8cc400fd2d894f757b9
Harry Carey <harry.carey95@gmail.com>
2023-05-23 17:46:21 +0200

    reformatted with black and added metadata loader

--- PyNutil/coordinate_extraction.py
+++ PyNutil/coordinate_extraction.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 import pandas as pd
 from DeepSlice.coord_post_processing.spacing_and_indexing import number_sections
@@ -12,21 +11,23 @@ import cv2
 from skimage import measure
 import threading
 
-#related to coordinate_extraction
+
+# related to coordinate_extraction
 def getCentroidsAndArea(Segmentation, pixelCutOff=0):
     """this function returns the center coordinate of each object in the segmentation.
-    You can set a pixelCutOff to remove objects that are smaller than that number of pixels"""
-    SegmentationBinary = ~np.all(Segmentation==255, axis=2) 
+    You can set a pixelCutOff to remove objects that are smaller than that number of pixels
+    """
+    SegmentationBinary = ~np.all(Segmentation == 255, axis=2)
     labels = measure.label(SegmentationBinary)
-    #this finds all the objects in the image
+    # this finds all the objects in the image
     labelsInfo = measure.regionprops(labels)
-    #remove objects that are less than pixelCutOff
+    # remove objects that are less than pixelCutOff
     labelsInfo = [label for label in labelsInfo if label.area > pixelCutOff]
-    #get the centre points of the objects
+    # get the centre points of the objects
     centroids = np.array([label.centroid for label in labelsInfo])
-    #get the area of the objects
+    # get the area of the objects
     area = np.array([label.area for label in labelsInfo])
-    #get the coordinates for all the pixels in each object
+    # get the coordinates for all the pixels in each object
     coords = np.array([label.coords for label in labelsInfo])
     return centroids, area, coords
 
@@ -34,31 +35,31 @@ def getCentroidsAndArea(Segmentation, pixelCutOff=0):
 # related to coordinate extraction
 def transformToRegistration(SegHeight, SegWidth, RegHeight, RegWidth):
     """this function returns the scaling factors to transform the segmentation to the registration space"""
-    Yscale = RegHeight/SegHeight
-    Xscale = RegWidth/SegWidth
-    return  Yscale,Xscale
+    Yscale = RegHeight / SegHeight
+    Xscale = RegWidth / SegWidth
+    return Yscale, Xscale
 
 
 # related to coordinate extraction
 def findMatchingPixels(Segmentation, id):
     """this function returns the Y and X coordinates of all the pixels in the segmentation that match the id provided"""
-    mask = Segmentation==id
+    mask = Segmentation == id
     mask = np.all(mask, axis=2)
     id_positions = np.where(mask)
-    idY, idX  = id_positions[0], id_positions[1]
-    return idY,idX
+    idY, idX = id_positions[0], id_positions[1]
+    return idY, idX
 
 
-#related to coordinate extraction
+# related to coordinate extraction
 def scalePositions(idY, idX, Yscale, Xscale):
     """this function scales the Y and X coordinates to the registration space.
-     (the Yscale and Xscale are the output of transformToRegistration)"""
+    (the Yscale and Xscale are the output of transformToRegistration)"""
     idY = idY * Yscale
     idX = idX * Xscale
-    return  idY,idX
+    return idY, idX
 
 
-#related to coordinate extraction
+# related to coordinate extraction
 def transformToAtlasSpace(anchoring, Y, X, RegHeight, RegWidth):
     """transform to atlas space using the QuickNII anchoring vector"""
     O = anchoring[0:3]
@@ -68,41 +69,43 @@ def transformToAtlasSpace(anchoring, Y, X, RegHeight, RegWidth):
     V = anchoring[6:9]
     # swap order of V
     V = np.array([V[0], V[1], V[2]])
-    #scale X and Y to between 0 and 1 using the registration width and height
-    Yscale = Y/RegHeight
-    Xscale = X/RegWidth
+    # scale X and Y to between 0 and 1 using the registration width and height
+    Yscale = Y / RegHeight
+    Xscale = X / RegWidth
     # print("width: ", RegWidth, " height: ", RegHeight, " Xmax: ", np.max(X), " Ymax: ", np.max(Y), " Xscale: ", np.max(Xscale), " Yscale: ", np.max(Yscale))
-    XYZV = np.array([Yscale*V[0], Yscale*V[1], Yscale*V[2]])
-    XYZU = np.array([Xscale*U[0], Xscale*U[1], Xscale*U[2]])
-    O = np.reshape(O, (3,1))
-    return (O+XYZU+XYZV).T
+    XYZV = np.array([Yscale * V[0], Yscale * V[1], Yscale * V[2]])
+    XYZU = np.array([Xscale * U[0], Xscale * U[1], Xscale * U[2]])
+    O = np.reshape(O, (3, 1))
+    return (O + XYZU + XYZV).T
 
 
 # related to coordinate extraction
-def SegmentationToAtlasSpace(slice, SegmentationPath, pixelID='auto', nonLinear=True):
+def SegmentationToAtlasSpace(slice, SegmentationPath, pixelID="auto", nonLinear=True):
     """combines many functions to convert a segmentation to atlas space. It takes care
     of deformations"""
     Segmentation = cv2.imread(SegmentationPath)
 
-    if pixelID == 'auto':
-        #remove the background from the segmentation
-        SegmentationNoBackGround = Segmentation[~np.all(Segmentation==255, axis=2)]
-        pixelID = np.vstack({tuple(r) for r in SegmentationNoBackGround.reshape(-1,3)})#remove background
-        #currently only works for a single label
+    if pixelID == "auto":
+        # remove the background from the segmentation
+        SegmentationNoBackGround = Segmentation[~np.all(Segmentation == 255, axis=2)]
+        pixelID = np.vstack(
+            {tuple(r) for r in SegmentationNoBackGround.reshape(-1, 3)}
+        )  # remove background
+        # currently only works for a single label
         pixelID = pixelID[0]
     ID_pixels = findMatchingPixels(Segmentation, pixelID)
-    #transform pixels to registration space (the registered image and segmentation have different dimensions)
+    # transform pixels to registration space (the registered image and segmentation have different dimensions)
     SegHeight = Segmentation.shape[0]
-    SegWidth  = Segmentation.shape[1]
+    SegWidth = Segmentation.shape[1]
     RegHeight = slice["height"]
-    RegWidth  = slice["width"]
-    #this calculates reg/seg
-    Yscale , Xscale = transformToRegistration(SegHeight,SegWidth,  RegHeight,RegWidth)
-    scaledY,scaledX = scalePositions(ID_pixels[0], ID_pixels[1], Yscale, Xscale)
+    RegWidth = slice["width"]
+    # this calculates reg/seg
+    Yscale, Xscale = transformToRegistration(SegHeight, SegWidth, RegHeight, RegWidth)
+    scaledY, scaledX = scalePositions(ID_pixels[0], ID_pixels[1], Yscale, Xscale)
     if nonLinear:
         if "markers" in slice:
-            #this creates a triangulation using the reg width
-            triangulation   = triangulate(RegWidth, RegHeight, slice["markers"])
+            # this creates a triangulation using the reg width
+            triangulation = triangulate(RegWidth, RegHeight, slice["markers"])
             newX, newY = transform_vec(triangulation, scaledX, scaledY)
         else:
             print(f"no markers found for " + slice["filename"])
@@ -110,9 +113,8 @@ def SegmentationToAtlasSpace(slice, SegmentationPath, pixelID='auto', nonLinear=
     else:
         newX, newY = scaledX, scaledY
 
-
-    #scale U by Uxyz/RegWidth and V by Vxyz/RegHeight
-    points = transformToAtlasSpace(slice['anchoring'], newY, newX, RegHeight, RegWidth)
+    # scale U by Uxyz/RegWidth and V by Vxyz/RegHeight
+    points = transformToAtlasSpace(slice["anchoring"], newY, newX, RegHeight, RegWidth)
     # points = points.reshape(-1)
     return np.array(points)
 
@@ -122,42 +124,67 @@ def FolderToAtlasSpace(folder, QUINT_alignment, pixelID=[0, 0, 0], nonLinear=Tru
     "apply Segmentation to atlas space to all segmentations in a folder"
     slices = loadVisuAlignJson(QUINT_alignment)
     points = []
-    segmentationFileTypes = [".png", ".tif", ".tiff", ".jpg", ".jpeg"] 
-    Segmentations = [file for file in glob(folder + "/*") if any([file.endswith(type) for type in segmentationFileTypes])]
+    segmentationFileTypes = [".png", ".tif", ".tiff", ".jpg", ".jpeg"]
+    Segmentations = [
+        file
+        for file in glob(folder + "/*")
+        if any([file.endswith(type) for type in segmentationFileTypes])
+    ]
     SectionNumbers = number_sections(Segmentations)
-    #order segmentations and sectionNumbers
+    # order segmentations and sectionNumbers
     # Segmentations = [x for _,x in sorted(zip(SectionNumbers,Segmentations))]
     # SectionNumbers.sort()
-    for  SegmentationPath in Segmentations:
+    for SegmentationPath in Segmentations:
         seg_nr = int(number_sections([SegmentationPath])[0])
-        current_slice_index = np.where([s["nr"]==seg_nr for s in slices])
+        current_slice_index = np.where([s["nr"] == seg_nr for s in slices])
         current_slice = slices[current_slice_index[0][0]]
         ##this converts the segmentation to a point cloud
-        points.extend(SegmentationToAtlasSpace(current_slice, SegmentationPath, pixelID, nonLinear))
+        points.extend(
+            SegmentationToAtlasSpace(
+                current_slice, SegmentationPath, pixelID, nonLinear
+            )
+        )
     return np.array(points)
 
-# points.append would make list of lists, keeping sections separate. 
+
+# points.append would make list of lists, keeping sections separate.
 
 
-#related to coordinate extraction
-#this function returns an array of points
-def FolderToAtlasSpaceMultiThreaded(folder, QUINT_alignment, pixelID=[0, 0, 0], nonLinear=True):
+# related to coordinate extraction
+# this function returns an array of points
+def FolderToAtlasSpaceMultiThreaded(
+    folder, QUINT_alignment, pixelID=[0, 0, 0], nonLinear=True
+):
     "apply Segmentation to atlas space to all segmentations in a folder"
     slices = loadVisuAlignJson(QUINT_alignment)
-    
-    segmentationFileTypes = [".png", ".tif", ".tiff", ".jpg", ".jpeg"] 
-    Segmentations = [file for file in glob(folder + "/*") if any([file.endswith(type) for type in segmentationFileTypes])]
+
+    segmentationFileTypes = [".png", ".tif", ".tiff", ".jpg", ".jpeg"]
+    Segmentations = [
+        file
+        for file in glob(folder + "/*")
+        if any([file.endswith(type) for type in segmentationFileTypes])
+    ]
     SectionNumbers = number_sections(Segmentations)
-    #order segmentations and sectionNumbers
+    # order segmentations and sectionNumbers
     # Segmentations = [x for _,x in sorted(zip(SectionNumbers,Segmentations))]
     # SectionNumbers.sort()
     pointsList = [None] * len(Segmentations)
     threads = []
-    for  SegmentationPath, index in zip(Segmentations, range(len(Segmentations))):
+    for SegmentationPath, index in zip(Segmentations, range(len(Segmentations))):
         seg_nr = int(number_sections([SegmentationPath])[0])
-        current_slice_index = np.where([s["nr"]==seg_nr for s in slices])
+        current_slice_index = np.where([s["nr"] == seg_nr for s in slices])
         current_slice = slices[current_slice_index[0][0]]
-        x = threading.Thread(target=SegmentationToAtlasSpaceMultiThreaded, args=(current_slice, SegmentationPath, pixelID, nonLinear, pointsList, index))
+        x = threading.Thread(
+            target=SegmentationToAtlasSpaceMultiThreaded,
+            args=(
+                current_slice,
+                SegmentationPath,
+                pixelID,
+                nonLinear,
+                pointsList,
+                index,
+            ),
+        )
         threads.append(x)
         ##this converts the segmentation to a point cloud
     # start threads
@@ -171,41 +198,43 @@ def FolderToAtlasSpaceMultiThreaded(folder, QUINT_alignment, pixelID=[0, 0, 0], 
 
 # related to coordinate extraction
 # this function returns an array of points
-def SegmentationToAtlasSpaceMultiThreaded(slice, SegmentationPath, pixelID='auto', nonLinear=True, pointsList=None, index=None):
+def SegmentationToAtlasSpaceMultiThreaded(
+    slice, SegmentationPath, pixelID="auto", nonLinear=True, pointsList=None, index=None
+):
     """combines many functions to convert a segmentation to atlas space. It takes care
     of deformations"""
     Segmentation = cv2.imread(SegmentationPath)
 
-    if pixelID == 'auto':
-        #remove the background from the segmentation
-        SegmentationNoBackGround = Segmentation[~np.all(Segmentation==255, axis=2)]
-        pixelID = np.vstack({tuple(r) for r in SegmentationNoBackGround.reshape(-1,3)})#remove background
-        #currently only works for a single label
+    if pixelID == "auto":
+        # remove the background from the segmentation
+        SegmentationNoBackGround = Segmentation[~np.all(Segmentation == 255, axis=2)]
+        pixelID = np.vstack(
+            {tuple(r) for r in SegmentationNoBackGround.reshape(-1, 3)}
+        )  # remove background
+        # currently only works for a single label
         pixelID = pixelID[0]
     ID_pixels = findMatchingPixels(Segmentation, pixelID)
-    #transform pixels to registration space (the registered image and segmentation have different dimensions)
+    # transform pixels to registration space (the registered image and segmentation have different dimensions)
     SegHeight = Segmentation.shape[0]
-    SegWidth  = Segmentation.shape[1]
+    SegWidth = Segmentation.shape[1]
     RegHeight = slice["height"]
-    RegWidth  = slice["width"]
-    #this calculates reg/seg
-    Yscale , Xscale = transformToRegistration(SegHeight,SegWidth,  RegHeight,RegWidth)
+    RegWidth = slice["width"]
+    # this calculates reg/seg
+    Yscale, Xscale = transformToRegistration(SegHeight, SegWidth, RegHeight, RegWidth)
 
-    #scale the seg coordinates to reg/seg
-    scaledY,scaledX = scalePositions(ID_pixels[0], ID_pixels[1], Yscale, Xscale)
+    # scale the seg coordinates to reg/seg
+    scaledY, scaledX = scalePositions(ID_pixels[0], ID_pixels[1], Yscale, Xscale)
     if nonLinear:
         if "markers" in slice:
-            #this creates a triangulation using the reg width
-            triangulation   = triangulate(RegWidth, RegHeight, slice["markers"])
+            # this creates a triangulation using the reg width
+            triangulation = triangulate(RegWidth, RegHeight, slice["markers"])
             newX, newY = transform_vec(triangulation, scaledX, scaledY)
         else:
             print(f"no markers found for " + slice["filename"])
             newX, newY = scaledX, scaledY
     else:
         newX, newY = scaledX, scaledY
-    #scale U by Uxyz/RegWidth and V by Vxyz/RegHeight
-    points = transformToAtlasSpace(slice['anchoring'], newY, newX, RegHeight, RegWidth)
+    # scale U by Uxyz/RegWidth and V by Vxyz/RegHeight
+    points = transformToAtlasSpace(slice["anchoring"], newY, newX, RegHeight, RegWidth)
     # points = points.reshape(-1)
     pointsList[index] = np.array(points)
-
-
--- PyNutil/counting_and_load.py
+++ PyNutil/counting_and_load.py
@@ -2,55 +2,56 @@ import numpy as np
 import pandas as pd
 import struct
 
+
 # related to counting and load
 def labelPoints(points, label_volume, scale_factor=1):
     """this function takes a list of points and assigns them to a region based on the regionVolume.
     These regions will just be the values in the regionVolume at the points.
     it returns a dictionary with the region as the key and the points as the value"""
-    #first convert the points to 3 columns
-    points = np.reshape(points, (-1,3))
-    #scale the points
+    # first convert the points to 3 columns
+    points = np.reshape(points, (-1, 3))
+    # scale the points
     points = points * scale_factor
-    #round the points to the nearest whole number
+    # round the points to the nearest whole number
     points = np.round(points).astype(int)
-    x = points[:,0]
-    y = points[:,1]
-    z = points[:,2]
-    #get the label value for each point
-    labels = label_volume[x,y,z]
+    x = points[:, 0]
+    y = points[:, 1]
+    z = points[:, 2]
+    # get the label value for each point
+    labels = label_volume[x, y, z]
     return labels
 
 
 # related to counting_and_load
-def PixelCountPerRegion(labelsDict, label_colours): 
-    """Function for counting no. of pixels per region and writing to CSV based on 
-    a dictionary with the region as the key and the points as the value, """
+def PixelCountPerRegion(labelsDict, label_colours):
+    """Function for counting no. of pixels per region and writing to CSV based on
+    a dictionary with the region as the key and the points as the value,"""
     counted_labels, label_counts = np.unique(labelsDict, return_counts=True)
     # which regions have pixels, and how many pixels are there per region
-    counts_per_label = list(zip(counted_labels,label_counts))
+    counts_per_label = list(zip(counted_labels, label_counts))
     # create a list of unique regions and pixel counts per region
 
-    df_counts_per_label = pd.DataFrame(counts_per_label, columns=["idx","pixel_count"])
+    df_counts_per_label = pd.DataFrame(counts_per_label, columns=["idx", "pixel_count"])
     # create a pandas df with regions and pixel counts
 
-    df_label_colours =pd.read_csv(label_colours, sep=",")
+    df_label_colours = pd.read_csv(label_colours, sep=",")
     # find colours corresponding to each region ID and add to the pandas dataframe
 
-    #look up name, r, g, b in df_allen_colours in df_counts_per_label based on "idx"
+    # look up name, r, g, b in df_allen_colours in df_counts_per_label based on "idx"
     new_rows = []
     for index, row in df_counts_per_label.iterrows():
-        mask = df_label_colours["idx"] == row["idx"] 
+        mask = df_label_colours["idx"] == row["idx"]
         current_region_row = df_label_colours[mask]
         current_region_name = current_region_row["name"].values
         current_region_red = current_region_row["r"].values
         current_region_green = current_region_row["g"].values
         current_region_blue = current_region_row["b"].values
 
-        row["name"]  = current_region_name[0]
+        row["name"] = current_region_name[0]
         row["r"] = current_region_red[0]
         row["g"] = current_region_green[0]
         row["b"] = current_region_blue[0]
-        
+
         new_rows.append(row)
 
     df_counts_per_label_name = pd.DataFrame(new_rows)
@@ -58,23 +59,25 @@ def PixelCountPerRegion(labelsDict, label_colours):
 
 
 """read flat file and write into an np array"""
-def flat_to_array(flatfile):
-    with open(flatfile,"rb") as f:
-        #i dont know what b is, w and h are the width and height that we get from the 
-        #flat file header
-        b,w,h=struct.unpack(">BII",f.read(9))
-        #data is a one dimensional list of values
-        #it has the shape width times height
-        data =struct.unpack(">"+("xBH"[b]*(w*h)),f.read(b*w*h))
 
-    #convert flat file data into an array, previously data was a tuple
+
+def flat_to_array(flatfile):
+    with open(flatfile, "rb") as f:
+        # i dont know what b is, w and h are the width and height that we get from the
+        # flat file header
+        b, w, h = struct.unpack(">BII", f.read(9))
+        # data is a one dimensional list of values
+        # it has the shape width times height
+        data = struct.unpack(">" + ("xBH"[b] * (w * h)), f.read(b * w * h))
+
+    # convert flat file data into an array, previously data was a tuple
     imagedata = np.array(data)
 
-    #create an empty image array in the right shape, write imagedata into image_array
-    image = np.zeros((h,w))
+    # create an empty image array in the right shape, write imagedata into image_array
+    image = np.zeros((h, w))
     for x in range(w):
         for y in range(h):
-            image[y,x] = imagedata[x+y*w]
+            image[y, x] = imagedata[x + y * w]
 
     image_arr = np.array(image)
     return image_arr
