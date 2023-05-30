cb9620b00a251b1f38f404d398cd4b2aa371ced2
sharoncy <s.c.yates@medisin.uio.no>
2023-05-30 13:48:03 +0200

    Perform testing with PyNutil test data

--- PyNutil/main.py
+++ PyNutil/main.py
@@ -272,7 +272,7 @@ class PyNutil:
             self.segmentation_filenames,
             self.per_section_df,
         ):
-            split_fn = fn.split("/")[-1].split(".")[0]
+            split_fn = fn.split(os.sep)[-1].split(".")[0]
             df.to_csv(
                 f"{output_folder}/per_section_reports/{split_fn}.csv",
                 sep=";",
