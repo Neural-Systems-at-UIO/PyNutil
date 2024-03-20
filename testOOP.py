dd1a8f0730cd2adc913902218e15d3b73767af5a
sharoncy <s.c.yates@medisin.uio.no>
2024-03-20 14:53:48 +0100

    Changed the scaling method for flat to segmentation
    The reports now give same region pixels as Nutil

--- testOOP.py
+++ testOOP.py
@@ -3,11 +3,13 @@ from PyNutil import PyNutil
 pnt = PyNutil(settings_file=r"PyNutil/test/test8_PyNutil_fixed.json")
 ##Use flat can be set to True if you want to use the flat file
 # instead of the visualign json (this is only useful for testing and will be removed)
-pnt.get_coordinates(object_cutoff=0, use_flat=False)
+pnt.get_coordinates(object_cutoff=0, use_flat=True)
 
 pnt.quantify_coordinates()
 
-pnt.save_analysis("PyNutil/outputs/test8_PyNutil")
+pnt.save_analysis("PyNutil/outputs/test8_PyNutil_bigcaudoputamen_newatlasmaps")
 
 # remove name, r, g, b, from pixel_
 # add to region_areas df
+
+
