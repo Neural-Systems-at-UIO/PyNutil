3ed5afaa2aaa0d4a5b1016ed4e5892ccc69abb11
Sharon Christine Yates <sharoncy@uio.no>
2024-03-25 13:26:54 +0100

    Add new linear tests
    PyNutil flat, PyNutil noflat and Nutil

--- testOOP.py
+++ testOOP.py
@@ -1,13 +1,13 @@
 from PyNutil import PyNutil
 
-pnt = PyNutil(settings_file=r"PyNutil/test/test8_PyNutil_bigcaudoputamen.json")
+pnt = PyNutil(settings_file=r"PyNutil/test/test9_PyNutil_linear_only.json")
 ##Use flat can be set to True if you want to use the flat file
 # instead of the visualign json (this is only useful for testing and will be removed)
 pnt.get_coordinates(object_cutoff=0, use_flat=False)
 
 pnt.quantify_coordinates()
 
-pnt.save_analysis("PyNutil/outputs/test8_PyNutil_bigcaudoputamen_noflat")
+pnt.save_analysis("PyNutil/outputs/test9_PyNutil_linear_noflat")
 
 # remove name, r, g, b, from pixel_
 # add to region_areas df
