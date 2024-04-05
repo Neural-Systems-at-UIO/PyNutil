41cb3cfb2fca56dbfa473ab50f4051d9a4e2f9f1
Sharon Christine Yates <sharoncy@uio.no>
2024-04-05 13:06:08 +0200

    Add tiling_script to scripts

--- testOOP.py
+++ testOOP.py
@@ -8,11 +8,11 @@ pnt = PyNutil(settings_file=r"PyNutil/test/test10_PyNutil_web.json")
 
 ##use_flat can be set to True if you want to use the flat file
 ## for method select between "all", "per_pixel" and "per_object"
-pnt.get_coordinates(object_cutoff=0, method="per_pixel", use_flat=False)
+pnt.get_coordinates(object_cutoff=0, method="all", use_flat=False)
 
 pnt.quantify_coordinates()
 
-pnt.save_analysis("PyNutil/outputs/test10_PyNutil_web")
+pnt.save_analysis("PyNutil/outputs/test10_PyNutil_web_all")
 
 # remove name, r, g, b, from pixel_
 # add to region_areas df
