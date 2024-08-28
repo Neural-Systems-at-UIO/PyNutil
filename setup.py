d4898fe98ea1685dcd523ceda23c5f0c57de7077
Harry Carey <38996929+PolarBean@users.noreply.github.com>
2024-08-28 12:39:21 +0200

    fix brainglobe name

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type='text/markdown',
     install_requires=[
         'numpy',
-        'brainglobe_atlasapi',
+        'brainglobe-atlasapi',
         'pandas',
         'requests',
         'pynrrd',
