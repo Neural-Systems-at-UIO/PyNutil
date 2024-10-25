94901c00ea1aeff5900025bfdf62865c0001abe8
sharoncy <s.c.yates@medisin.uio.no>
2024-10-25 10:08:59 +0200

    Minor updates to testing file
    Updated the workflow for running a test using a custom atlas

--- workflows/basic_example_custom_atlas.py
+++ workflows/basic_example_custom_atlas.py
@@ -9,15 +9,17 @@ from PyNutil import PyNutil
 ###The segmentations should be images which come out of ilastix, segmenting an object of interest
 ###The alignment json should be out of DeepSlice, QuickNII, or VisuAlign, it defines the sections position in an atlas
 ###The colour says which colour is the object you want to quantify in your segmentation. It is defined in RGB
-###Finally the atlas name is the relevant atlas from brainglobe_atlasapi you wish to use in Quantification.
+###The atlas_path is the path to the relevant atlas.nrrd
+###The label_path is the path to the corresponding atlas .csv
+###The object_cutoff is a cut-off for min object size
 
 pnt = PyNutil(
     segmentation_folder="../tests/test_data/nonlinear_allen_mouse/",
     alignment_json="../tests/test_data/nonlinear_allen_mouse/alignment.json",
     colour=[0, 0, 0],
-    atlas_path="/mnt/z/HBP_analytics/PyNutil/metadata/annotation_volumes/annotation_25_reoriented_2017.nrrd",
-    label_path="/mnt/z/HBP_analytics/PyNutil/metadata/annotation_volumes/allen2017_colours.csv",
+    atlas_path="../tests/test_data/allen_mouse_2017_atlas/annotation_25_reoriented_2017.nrrd",
+    label_path="../tests/test_data/allen_mouse_2017_atlas//allen2017_colours.csv",
 )
 pnt.get_coordinates(object_cutoff=0)
 pnt.quantify_coordinates()
-pnt.save_analysis("../demo_data/PyNutil_nonlinear_noflat_custom")
+pnt.save_analysis("../demo_data/test_nonlinear_allen_mouse")
