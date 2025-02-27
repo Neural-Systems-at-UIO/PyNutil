c34c0c34225e1b414f1677950c92a92d8740e315
polarbean <harry.carey95@gmail.com>
2025-02-27 14:24:16 +0100

    update tests

--- workflows/basic_example.py
+++ workflows/basic_example.py
@@ -11,12 +11,15 @@ from PyNutil import PyNutil
 ###The colour says which colour is the object you want to quantify in your segmentation. It is defined in RGB
 ###Finally the atlas name is the relevant atlas from brainglobe_atlasapi you wish to use in Quantification.
 ### get_coordinates, if use_flat=True, place flat files in folder titled "flat_files" at same level as "segmentations" folder .flat or .seg
+script_dir = os.path.dirname(os.path.abspath(__file__))
 
 pnt = PyNutil(
-    segmentation_folder="../tests/test_data/nonlinear_allen_mouse/",
-    alignment_json="../tests/test_data/nonlinear_allen_mouse/alignment.json",
+    segmentation_folder=os.path.join(script_dir, "../tests/test_data/nonlinear_allen_mouse/"),
+    alignment_json=os.path.join(script_dir, "../tests/test_data/nonlinear_allen_mouse/alignment.json"),
     colour=[0, 0, 0],
     atlas_name="allen_mouse_25um",
 )
-pnt.get_coordinates(object_cutoff=0, use_flat=True)
+
+pnt.get_coordinates(object_cutoff=0, use_flat=False)
 pnt.quantify_coordinates()
+pnt.save_analysis("../test_result/bg_test")
