089ee6509e6fcc3db0e9eb1bb21dfa93f1c64e55
polarbean <harry.carey95@gmail.com>
2026-03-17 19:08:52 +0000

    define __all__ for more sensible imports

--- PyNutil/__init__.py
+++ PyNutil/__init__.py
@@ -1,13 +1,29 @@
 from .results import AtlasData, ExtractionResult
 from .processing.adapters.base import RegistrationData
-from .processing.adapters import load_registration as read_alignment
+from .processing.adapters import read_alignment
 from .io.atlas_loader import load_atlas_data, load_custom_atlas
 from .processing.pipeline.batch_processor import (
-    folder_to_atlas_space as seg_to_coords,
-    folder_to_atlas_space_intensity as image_to_coords,
-    file_to_atlas_space_coordinates as xy_to_coords,
+    seg_to_coords,
+    image_to_coords,
+    xy_to_coords,
 )
 from .processing.analysis.data_analysis import quantify_coords
 from .io.file_operations import save_analysis
-from .processing.section_volume import project_sections_to_volume as interpolate_volume
+from .processing.section_volume import interpolate_volume
 from .io.volume_nifti import save_volume_niftis
+
+__all__ = [
+    "AtlasData",
+    "ExtractionResult",
+    "RegistrationData",
+    "read_alignment",
+    "load_atlas_data",
+    "load_custom_atlas",
+    "seg_to_coords",
+    "image_to_coords",
+    "xy_to_coords",
+    "quantify_coords",
+    "save_analysis",
+    "interpolate_volume",
+    "save_volume_niftis",
+]
